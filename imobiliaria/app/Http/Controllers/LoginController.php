032f522927baf702441fdbe8ccfc265fb7cace57
matt <mateush.study@gmail.com>
2024-08-21 09:27:29 -0300

    Login controller

---
+++ imobiliaria/app/Http/Controllers/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController
+{
+    public function showLoginForm()
+    {
+        return view('login');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            // Authentication passed...
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        return redirect('/login');
+    }
+}
--- imobiliaria/resources/views/login.blade.php
+++ imobiliaria/resources/views/login.blade.php
@@ -6,15 +6,15 @@
 
     <div class="loginPage">
     <div class="login-container">
-        <form class="login-form">
+        <form class="login-form" method="POST" action="{{ route('login') }}">
             <h2 class="text-center">LOGIN</h2>
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" placeholder="Digite seu email">
+                <input type="email" class="form-control" name="email" id="email" placeholder="Digite seu email">
             </div>
             <div class="mb-3">
                 <label for="senha" class="form-label">Senha</label>
-                <input type="password" class="form-control" id="senha" placeholder="Digite sua senha">
+                <input type="password" class="form-control" name="password" id="password" placeholder="Digite sua senha">
             </div>
             <div class="mb-3 d-flex justify-content-between">
                 <div class="form-check">
