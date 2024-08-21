ff19ca023457a29114566fcdd181a4b702166bc3
Matheus Peixoto <mateusnascimentopxt@gmail.com>
2024-08-21 20:20:18 -0300

    página corretor e teste Home

--- imobiliaria/routes/web.php
+++ imobiliaria/routes/web.php
@@ -29,3 +29,7 @@ Route::get('/imoveis', function () {
 Route::get('/contato', function () {
     return view('contato');
 });
+
+Route::get('/corretor', function () {
+    return view('corretor');
+});
