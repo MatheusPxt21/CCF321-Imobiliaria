da8bfc91813d212c31d70738e874dacdee2b178b
Matheus Peixoto <mateusnascimentopxt@gmail.com>
2024-09-03 23:15:12 -0300

    home bunitinha

--- imobiliaria/app/Http/Controllers/HomeController.php
+++ imobiliaria/app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ class HomeController extends Controller
     public function index()
     {
         // Busque 5 imóveis em destaque, por exemplo, ordenados por data de criação ou qualquer outro critério
-        $destaques = Imovel::orderBy('created_at', 'desc')->take(5)->get();
+        $destaques = Imovel::orderBy('created_at', 'desc')->take(6)->get();
 
         return view('welcome', compact('destaques'));
     }
--- imobiliaria/resources/views/welcome.blade.php
+++ imobiliaria/resources/views/welcome.blade.php
@@ -8,38 +8,35 @@
 
     <!-- Seção de Imagem Principal -->
     <section class="hero-section">
-        <div class="img-hero-section">
-            <div class="banner">
-                <img src="/imgs/banner.png" class="img-fluid">
-                <div class="social-icons">
-                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                    <a href="#"><i class="fab fa-instagram"></i></a>
-                    <a href="#"><i class="fab fa-twitter"></i></a>
+        <div class="hero-content" style="margin-inline: 10rem;">
+            <div class="home_img-hero-section">
+                <div class="home_banner">
+                    <img src="/imgs/banner.png" class="img-fluid">
                 </div>
-            <div>
-        </div>
-        <div class="hero-text">
-            <h1>Construindo Lares</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel massa pulvinar, scelerisque odio sed, sodales sapien. Quisque vehicula lectus nec nunc dapibus, vitae sagittis velit ultrices.</p>
-            <a href="#" class="btn btn-custom">Saiba Mais</a>
+            </div>
+            <div class="home_hero-text">
+                <h1>Construindo Lares</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel massa pulvinar, scelerisque odio sed, sodales sapien. Quisque vehicula lectus nec nunc dapibus, vitae sagittis velit ultrices.</p>
+                <a href="/contato" class="btn btn-custom">Saiba Mais</a>
+            </div>
         </div>
     </section>
 
     <!-- Seção de Destaques -->
-    <section class="highlights-section">
+    <section class="home_highlight-section">
         <h2>Destaques</h2>
-        <div class="highlights-cards">
-            <div class="row highlight-card-row">
+        <div class="home_highlights-cards">
+            <div class="row home_highlight-card-row">
                 @foreach($destaques as $imovel)
-                    <div class="col-md-4 highlight-card">
-                        <div class="highlight-item">
+                    <div class="col-md-4 home_highlight-card">
+                        <div class="home_highlight-item">
                             <!-- Exibe a primeira imagem do imóvel, se existir -->
                             @if($imovel->imagens->isNotEmpty())
                                 <img src="{{ asset('storage/' . $imovel->imagens->first()->caminho_imagem) }}" class="img-fluid">
                             @else
                                 <img src="/imgs/default.jpg" class="img-fluid" alt="Imagem do imóvel">
                             @endif
-                            <div class="highlight-text">
+                            <div class="home_highlight-text">
                                 <h4>{{ $imovel->titulo }}</h4>
                                 <p>{{ \Illuminate\Support\Str::limit($imovel->descricao, 50) }}</p>
                                 <a href="{{ route('imovel.show', $imovel->id) }}" class="btn btn-custom">Saiba Mais</a>
@@ -51,18 +48,6 @@
         </div>
     </section>
 
-    <!-- Seção de Texto e Imagem -->
-    <section class="text-image-section">
-        <div class="row">
-            <div class="col-md-8">
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel massa pulvinar, scelerisque odio sed, sodales sapien. Quisque vehicula lectus nec nunc dapibus, vitae sagittis velit ultrices.</p>
-            </div>
-            <div class="col-md-4">
-                <img src="/imgs/building.jpg" class="img-fluid">
-            </div>
-        </div>
-    </section>
 </div>
 
 @endsection
